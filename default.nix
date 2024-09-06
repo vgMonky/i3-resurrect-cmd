f6fd93e10cdf491614dc7f02eb752e94c602aa7d
Vik <victorgou.design@gmail.com>
2024-09-06 17:19:39 -0300

    fixed default.nix

--- default.nix
+++ default.nix
@@ -1,6 +1,5 @@
 { lib
 , python3Packages
-, fetchFromGitHub
 , i3
 , xdotool
 , xorg
@@ -11,12 +10,7 @@ python3Packages.buildPythonApplication rec {
   pname = "i3-resurrect-cmd";
   version = "0.1.0-dev";
 
-  src = fetchFromGitHub {
-    owner = "vgMonky";
-    repo = "i3-resurrect-cmd";
-    rev = "v${version}";
-    sha256 = "sha256-fVD7CEKaj864EcsEZ/gWITg9X1JuLQWelJajw0vyFHA="; 
-  };
+  src = ./.;
 
   propagatedBuildInputs = with python3Packages; [
     click
@@ -33,7 +27,7 @@ python3Packages.buildPythonApplication rec {
   meta = with lib; {
     description = "A command-line interface for i3-resurrect";
     homepage = "https://github.com/vgMonky/i3-resurrect-cmd";
-    license = licenses.mit;  # Adjust if you're using a different license
+    license = licenses.mit;
     maintainers = with maintainers; [ vgMonky ];
   };
 }
